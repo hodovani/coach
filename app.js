01e2cb4fe3a631dd4b99339b00f05b01962b676f
Matvii Hodovaniuk <matvii@hodovani.uk>
2024-01-23 14:05:08 +0200

    move exercises to separate file

--- app.js
+++ app.js
@@ -1,189 +1,6 @@
+import workoutList from './workoutList.js';
+
 window.addEventListener('load', function () {
-  const workoutList = [
-    { message: 'Good luck!', time: 5 },
-
-    { message: 'Next exercise is Jumping Jacks in 5 seconds', time: 10 },
-    { message: 'Start', time: 30, shouldNotifyHalfTime: true, shouldPlaySound: true },
-    { message: 'End', time: 5 },
-    { message: 'Relax 10 seconds', time: 10 },
-
-    { message: 'Next exercise is Leg Swings in 5 seconds', time: 10 },
-    { message: 'Start', time: 30, shouldNotifyHalfTime: true, shouldPlaySound: true },
-    { message: 'End', time: 5 },
-    { message: 'Relax 10 seconds', time: 10 },
-
-    { message: 'Next exercise is Hip Circles in 5 seconds', time: 10 },
-    { message: 'Start', time: 30, shouldNotifyHalfTime: true, shouldPlaySound: true },
-    { message: 'End', time: 5 },
-    { message: 'Relax 10 seconds', time: 10 },
-
-    { message: 'Next exercise is High Knees in 5 seconds', time: 10 },
-    { message: 'Start', time: 30, shouldNotifyHalfTime: true, shouldPlaySound: true },
-    { message: 'End', time: 5 },
-    { message: 'Relax 10 seconds', time: 10 },
-
-    { message: 'Next exercise is Butt Kicks in 5 seconds', time: 10 },
-    { message: 'Start', time: 30, shouldNotifyHalfTime: true, shouldPlaySound: true },
-    { message: 'End', time: 5 },
-    { message: 'Relax 10 seconds', time: 10 },
-
-    { message: 'Next exercise is Jump Rope in 5 seconds', time: 10 },
-    { message: 'Start', time: 30, shouldNotifyHalfTime: true, shouldPlaySound: true },
-    { message: 'End', time: 5 },
-    { message: 'Relax 10 seconds', time: 10 },
-
-    { message: 'Next exercise is Torso Twists in 5 seconds', time: 10 },
-    { message: 'Start', time: 30, shouldNotifyHalfTime: true, shouldPlaySound: true },
-    { message: 'End', time: 5 },
-    { message: 'Relax 10 seconds', time: 10 },
-
-    { message: 'Next exercise is Ankle Rolls in 5 seconds', time: 10 },
-    { message: 'Start', time: 30, shouldNotifyHalfTime: true, shouldPlaySound: true },
-    { message: 'End', time: 5 },
-    { message: 'Relax 10 seconds', time: 10 },
-
-    { message: 'Next exercise is Dynamic Lunges in 5 seconds', time: 10 },
-    { message: 'Start', time: 30, shouldNotifyHalfTime: true, shouldPlaySound: true },
-    { message: 'End', time: 5 },
-
-    { message: 'Next exercise is Wrist Circles in 5 seconds', time: 10 },
-    { message: 'Start', time: 30, shouldNotifyHalfTime: true, shouldPlaySound: true },
-    { message: 'End', time: 5 },
-    { message: 'Relax 10 seconds', time: 10 },
-
-    { message: 'Next exercise is Neck Tilts and Rolls in 5 seconds', time: 10 },
-    { message: 'Start', time: 30, shouldNotifyHalfTime: true, shouldPlaySound: true },
-    { message: 'End', time: 5 },
-    { message: 'Relax 10 seconds', time: 10 },
-
-    { message: 'Next exercise is Side Bends in 5 seconds', time: 10 },
-    { message: 'Start', time: 30, shouldNotifyHalfTime: true, shouldPlaySound: true },
-    { message: 'End', time: 5 },
-
-    { message: 'Relax 30 seconds before main workout', time: 30, shouldNotifyHalfTime: true },
-
-    { message: 'Next exercise is Bodyweight Squats in 5 seconds', time: 10 },
-    { message: 'Start', time: 30, shouldNotifyHalfTime: true, shouldPlaySound: true },
-    { message: 'End', time: 5 },
-    { message: 'Relax 10 seconds', time: 10 },
-
-    { message: 'Next exercise is Lunges in 5 seconds', time: 10 },
-    { message: 'Start', time: 30, shouldNotifyHalfTime: true, shouldPlaySound: true },
-    { message: 'End', time: 5 },
-    { message: 'Relax 10 seconds', time: 10 },
-
-    { message: 'Next exercise is Push-ups in 5 seconds', time: 10 },
-    { message: 'Start', time: 30, shouldNotifyHalfTime: true, shouldPlaySound: true },
-    { message: 'End', time: 5 },
-    { message: 'Relax 10 seconds', time: 10 },
-
-    { message: 'Next exercise is Tricep Dips in 5 seconds', time: 10 },
-    { message: 'Start', time: 30, shouldNotifyHalfTime: true, shouldPlaySound: true },
-    { message: 'End', time: 5 },
-    { message: 'Relax 10 seconds', time: 10 },
-
-    { message: 'Next exercise is Bodyweight Rows in 5 seconds', time: 10 },
-    { message: 'Start', time: 30, shouldNotifyHalfTime: true, shouldPlaySound: true },
-    { message: 'End', time: 5 },
-    { message: 'Relax 10 seconds', time: 10 },
-
-    { message: 'Next exercise is Mountain Climbers in 5 seconds', time: 10 },
-    { message: 'Start', time: 30, shouldNotifyHalfTime: true, shouldPlaySound: true },
-    { message: 'End', time: 5 },
-    { message: 'Relax 10 seconds', time: 10 },
-
-    { message: 'Next exercise is Burpees in 5 seconds', time: 10 },
-    { message: 'Start', time: 30, shouldNotifyHalfTime: true, shouldPlaySound: true },
-    { message: 'End', time: 5 },
-    { message: 'Relax 10 seconds', time: 10 },
-
-    { message: 'Next exercise is Bicycle crunch in 5 seconds', time: 10 },
-    { message: 'Start', time: 30, shouldNotifyHalfTime: true, shouldPlaySound: true },
-    { message: 'End', time: 5 },
-    { message: 'Relax 10 seconds', time: 10 },
-
-    { message: 'Next exercise is Calf Raises in 5 seconds', time: 10 },
-    { message: 'Start', time: 30, shouldNotifyHalfTime: true, shouldPlaySound: true },
-    { message: 'End', time: 5 },
-    { message: 'Relax 10 seconds', time: 10 },
-
-    { message: 'Next exercise is Plank in 5 seconds', time: 10 },
-    { message: 'Start', time: 30, shouldNotifyHalfTime: true, shouldPlaySound: true },
-    { message: 'End', time: 5 },
-
-    { message: 'Relax', time: 30 },
-
-    { message: 'Next exercise is Neck Stretch in 5 seconds', time: 10 },
-    { message: 'Start', time: 30, shouldNotifyHalfTime: true, shouldPlaySound: true },
-    { message: 'End', time: 5 },
-    { message: 'Relax 10 seconds', time: 10 },
-
-    { message: 'Next exercise is Shoulder Stretch in 5 seconds', time: 10 },
-    { message: 'Start', time: 30, shouldNotifyHalfTime: true, shouldPlaySound: true },
-    { message: 'End', time: 5 },
-    { message: 'Relax 10 seconds', time: 10 },
-
-    { message: 'Next exercise is Triceps Stretch in 5 seconds', time: 10 },
-    { message: 'Start', time: 30, shouldNotifyHalfTime: true, shouldPlaySound: true },
-    { message: 'End', time: 5 },
-    { message: 'Relax 10 seconds', time: 10 },
-
-    { message: 'Next exercise is Chest Opener in 5 seconds', time: 10 },
-    { message: 'Start', time: 30, shouldNotifyHalfTime: true, shouldPlaySound: true },
-    { message: 'End', time: 5 },
-    { message: 'Relax 10 seconds', time: 10 },
-
-    { message: 'Next exercise is Wrist Stretch in 5 seconds', time: 10 },
-    { message: 'Start', time: 30, shouldNotifyHalfTime: true, shouldPlaySound: true },
-    { message: 'End', time: 5 },
-    { message: 'Relax 10 seconds', time: 10 },
-
-    { message: 'Next exercise is Cat-Cow Stretch in 5 seconds', time: 10 },
-    { message: 'Start', time: 30, shouldNotifyHalfTime: true, shouldPlaySound: true },
-    { message: 'End', time: 5 },
-    { message: 'Relax 10 seconds', time: 10 },
-
-    { message: 'Next exercise is Child Pose in 5 seconds', time: 10 },
-    { message: 'Start', time: 30, shouldNotifyHalfTime: true, shouldPlaySound: true },
-    { message: 'End', time: 5 },
-    { message: 'Relax 10 seconds', time: 10 },
-
-    { message: 'Next exercise is Seated Forward Bend in 5 seconds', time: 10 },
-    { message: 'Start', time: 30, shouldNotifyHalfTime: true, shouldPlaySound: true },
-    { message: 'Relax 10 seconds', time: 10 },
-
-    { message: 'Next exercise is Butterfly Stretch in 5 seconds', time: 10 },
-    { message: 'Start', time: 30, shouldNotifyHalfTime: true, shouldPlaySound: true },
-    { message: 'End', time: 5 },
-    { message: 'Relax 10 seconds', time: 10 },
-
-    { message: 'Next exercise is Quadriceps Stretch in 5 seconds', time: 10 },
-    { message: 'Start', time: 30, shouldNotifyHalfTime: true, shouldPlaySound: true },
-    { message: 'End', time: 5 },
-    { message: 'Relax 10 seconds', time: 10 },
-
-    { message: 'Next exercise is Hamstring Stretch in 5 seconds', time: 10 },
-    { message: 'Start', time: 30, shouldNotifyHalfTime: true, shouldPlaySound: true },
-    { message: 'End', time: 5 },
-    { message: 'Relax 10 seconds', time: 10 },
-
-    { message: 'Next exercise is Inner Thigh Stretch in 5 seconds', time: 10 },
-    { message: 'Start', time: 30, shouldNotifyHalfTime: true, shouldPlaySound: true },
-    { message: 'End', time: 5 },
-    { message: 'Relax 10 seconds', time: 10 },
-
-    { message: 'Next exercise is Calf Stretch in 5 seconds', time: 10 },
-    { message: 'Start', time: 30, shouldNotifyHalfTime: true, shouldPlaySound: true },
-    { message: 'End', time: 5 },
-    { message: 'Relax 10 seconds', time: 10 },
-
-    { message: 'Next exercise is Spinal twist in 5 seconds', time: 10 },
-    { message: 'Start', time: 30, shouldNotifyHalfTime: true, shouldPlaySound: true },
-    { message: 'End', time: 5 },
-
-    { message: 'Good job!', time: 5 },
-  ];
-
   let currentIndex = 0;
   let state = 'Play'; // Initial state
   let countdownInterval;
@@ -287,7 +104,18 @@ window.addEventListener('load', function () {
     speakNextWithDelay();
   }
 
+  const unlockAudioContext = (audioCtx) => {
+    if (audioCtx.state !== 'suspended') return;
+    const b = document.body;
+    const events = ['touchstart', 'touchend', 'mousedown', 'keydown'];
+    events.forEach(e => b.addEventListener(e, unlock, false));
+    function unlock() { audioCtx.resume().then(clean); }
+    function clean() { events.forEach(e => b.removeEventListener(e, unlock)); }
+  }
+
   function playBeat() {
+    unlockAudioContext(audioContext);
+
     const gainNode = audioContext.createGain();
     const oscillator = audioContext.createOscillator();
 
---
+++ workoutList.js
@@ -0,0 +1,649 @@
+const list = [
+  {
+    "message": "Good luck!",
+    "time": 5
+  },
+  {
+    "message": "Next exercise is Jumping Jacks in 5 seconds",
+    "time": 10
+  },
+  {
+    "message": "Start",
+    "time": 30,
+    "shouldNotifyHalfTime": true,
+    "shouldPlaySound": true
+  },
+  {
+    "message": "End",
+    "time": 5
+  },
+  {
+    "message": "Relax 10 seconds",
+    "time": 10
+  },
+  {
+    "message": "Next exercise is Leg Swings in 5 seconds",
+    "time": 10
+  },
+  {
+    "message": "Start",
+    "time": 30,
+    "shouldNotifyHalfTime": true,
+    "shouldPlaySound": true
+  },
+  {
+    "message": "End",
+    "time": 5
+  },
+  {
+    "message": "Relax 10 seconds",
+    "time": 10
+  },
+  {
+    "message": "Next exercise is Hip Circles in 5 seconds",
+    "time": 10
+  },
+  {
+    "message": "Start",
+    "time": 30,
+    "shouldNotifyHalfTime": true,
+    "shouldPlaySound": true
+  },
+  {
+    "message": "End",
+    "time": 5
+  },
+  {
+    "message": "Relax 10 seconds",
+    "time": 10
+  },
+  {
+    "message": "Next exercise is High Knees in 5 seconds",
+    "time": 10
+  },
+  {
+    "message": "Start",
+    "time": 30,
+    "shouldNotifyHalfTime": true,
+    "shouldPlaySound": true
+  },
+  {
+    "message": "End",
+    "time": 5
+  },
+  {
+    "message": "Relax 10 seconds",
+    "time": 10
+  },
+  {
+    "message": "Next exercise is Butt Kicks in 5 seconds",
+    "time": 10
+  },
+  {
+    "message": "Start",
+    "time": 30,
+    "shouldNotifyHalfTime": true,
+    "shouldPlaySound": true
+  },
+  {
+    "message": "End",
+    "time": 5
+  },
+  {
+    "message": "Relax 10 seconds",
+    "time": 10
+  },
+  {
+    "message": "Next exercise is Jump Rope in 5 seconds",
+    "time": 10
+  },
+  {
+    "message": "Start",
+    "time": 30,
+    "shouldNotifyHalfTime": true,
+    "shouldPlaySound": true
+  },
+  {
+    "message": "End",
+    "time": 5
+  },
+  {
+    "message": "Relax 10 seconds",
+    "time": 10
+  },
+  {
+    "message": "Next exercise is Torso Twists in 5 seconds",
+    "time": 10
+  },
+  {
+    "message": "Start",
+    "time": 30,
+    "shouldNotifyHalfTime": true,
+    "shouldPlaySound": true
+  },
+  {
+    "message": "End",
+    "time": 5
+  },
+  {
+    "message": "Relax 10 seconds",
+    "time": 10
+  },
+  {
+    "message": "Next exercise is Ankle Rolls in 5 seconds",
+    "time": 10
+  },
+  {
+    "message": "Start",
+    "time": 30,
+    "shouldNotifyHalfTime": true,
+    "shouldPlaySound": true
+  },
+  {
+    "message": "End",
+    "time": 5
+  },
+  {
+    "message": "Relax 10 seconds",
+    "time": 10
+  },
+  {
+    "message": "Next exercise is Dynamic Lunges in 5 seconds",
+    "time": 10
+  },
+  {
+    "message": "Start",
+    "time": 30,
+    "shouldNotifyHalfTime": true,
+    "shouldPlaySound": true
+  },
+  {
+    "message": "End",
+    "time": 5
+  },
+  {
+    "message": "Next exercise is Wrist Circles in 5 seconds",
+    "time": 10
+  },
+  {
+    "message": "Start",
+    "time": 30,
+    "shouldNotifyHalfTime": true,
+    "shouldPlaySound": true
+  },
+  {
+    "message": "End",
+    "time": 5
+  },
+  {
+    "message": "Relax 10 seconds",
+    "time": 10
+  },
+  {
+    "message": "Next exercise is Neck Tilts and Rolls in 5 seconds",
+    "time": 10
+  },
+  {
+    "message": "Start",
+    "time": 30,
+    "shouldNotifyHalfTime": true,
+    "shouldPlaySound": true
+  },
+  {
+    "message": "End",
+    "time": 5
+  },
+  {
+    "message": "Relax 10 seconds",
+    "time": 10
+  },
+  {
+    "message": "Next exercise is Side Bends in 5 seconds",
+    "time": 10
+  },
+  {
+    "message": "Start",
+    "time": 30,
+    "shouldNotifyHalfTime": true,
+    "shouldPlaySound": true
+  },
+  {
+    "message": "End",
+    "time": 5
+  },
+  {
+    "message": "Relax 30 seconds before main workout",
+    "time": 30,
+    "shouldNotifyHalfTime": true
+  },
+  {
+    "message": "Next exercise is Bodyweight Squats in 5 seconds",
+    "time": 10
+  },
+  {
+    "message": "Start",
+    "time": 30,
+    "shouldNotifyHalfTime": true,
+    "shouldPlaySound": true
+  },
+  {
+    "message": "End",
+    "time": 5
+  },
+  {
+    "message": "Relax 10 seconds",
+    "time": 10
+  },
+  {
+    "message": "Next exercise is Lunges in 5 seconds",
+    "time": 10
+  },
+  {
+    "message": "Start",
+    "time": 30,
+    "shouldNotifyHalfTime": true,
+    "shouldPlaySound": true
+  },
+  {
+    "message": "End",
+    "time": 5
+  },
+  {
+    "message": "Relax 10 seconds",
+    "time": 10
+  },
+  {
+    "message": "Next exercise is Push-ups in 5 seconds",
+    "time": 10
+  },
+  {
+    "message": "Start",
+    "time": 30,
+    "shouldNotifyHalfTime": true,
+    "shouldPlaySound": true
+  },
+  {
+    "message": "End",
+    "time": 5
+  },
+  {
+    "message": "Relax 10 seconds",
+    "time": 10
+  },
+  {
+    "message": "Next exercise is Tricep Dips in 5 seconds",
+    "time": 10
+  },
+  {
+    "message": "Start",
+    "time": 30,
+    "shouldNotifyHalfTime": true,
+    "shouldPlaySound": true
+  },
+  {
+    "message": "End",
+    "time": 5
+  },
+  {
+    "message": "Relax 10 seconds",
+    "time": 10
+  },
+  {
+    "message": "Next exercise is Bodyweight Rows in 5 seconds",
+    "time": 10
+  },
+  {
+    "message": "Start",
+    "time": 30,
+    "shouldNotifyHalfTime": true,
+    "shouldPlaySound": true
+  },
+  {
+    "message": "End",
+    "time": 5
+  },
+  {
+    "message": "Relax 10 seconds",
+    "time": 10
+  },
+  {
+    "message": "Next exercise is Mountain Climbers in 5 seconds",
+    "time": 10
+  },
+  {
+    "message": "Start",
+    "time": 30,
+    "shouldNotifyHalfTime": true,
+    "shouldPlaySound": true
+  },
+  {
+    "message": "End",
+    "time": 5
+  },
+  {
+    "message": "Relax 10 seconds",
+    "time": 10
+  },
+  {
+    "message": "Next exercise is Burpees in 5 seconds",
+    "time": 10
+  },
+  {
+    "message": "Start",
+    "time": 30,
+    "shouldNotifyHalfTime": true,
+    "shouldPlaySound": true
+  },
+  {
+    "message": "End",
+    "time": 5
+  },
+  {
+    "message": "Relax 10 seconds",
+    "time": 10
+  },
+  {
+    "message": "Next exercise is Bicycle crunch in 5 seconds",
+    "time": 10
+  },
+  {
+    "message": "Start",
+    "time": 30,
+    "shouldNotifyHalfTime": true,
+    "shouldPlaySound": true
+  },
+  {
+    "message": "End",
+    "time": 5
+  },
+  {
+    "message": "Relax 10 seconds",
+    "time": 10
+  },
+  {
+    "message": "Next exercise is Calf Raises in 5 seconds",
+    "time": 10
+  },
+  {
+    "message": "Start",
+    "time": 30,
+    "shouldNotifyHalfTime": true,
+    "shouldPlaySound": true
+  },
+  {
+    "message": "End",
+    "time": 5
+  },
+  {
+    "message": "Relax 10 seconds",
+    "time": 10
+  },
+  {
+    "message": "Next exercise is Plank in 5 seconds",
+    "time": 10
+  },
+  {
+    "message": "Start",
+    "time": 30,
+    "shouldNotifyHalfTime": true,
+    "shouldPlaySound": true
+  },
+  {
+    "message": "End",
+    "time": 5
+  },
+  {
+    "message": "Relax",
+    "time": 30
+  },
+  {
+    "message": "Next exercise is Neck Stretch in 5 seconds",
+    "time": 10
+  },
+  {
+    "message": "Start",
+    "time": 30,
+    "shouldNotifyHalfTime": true,
+    "shouldPlaySound": true
+  },
+  {
+    "message": "End",
+    "time": 5
+  },
+  {
+    "message": "Relax 10 seconds",
+    "time": 10
+  },
+  {
+    "message": "Next exercise is Shoulder Stretch in 5 seconds",
+    "time": 10
+  },
+  {
+    "message": "Start",
+    "time": 30,
+    "shouldNotifyHalfTime": true,
+    "shouldPlaySound": true
+  },
+  {
+    "message": "End",
+    "time": 5
+  },
+  {
+    "message": "Relax 10 seconds",
+    "time": 10
+  },
+  {
+    "message": "Next exercise is Triceps Stretch in 5 seconds",
+    "time": 10
+  },
+  {
+    "message": "Start",
+    "time": 30,
+    "shouldNotifyHalfTime": true,
+    "shouldPlaySound": true
+  },
+  {
+    "message": "End",
+    "time": 5
+  },
+  {
+    "message": "Relax 10 seconds",
+    "time": 10
+  },
+  {
+    "message": "Next exercise is Chest Opener in 5 seconds",
+    "time": 10
+  },
+  {
+    "message": "Start",
+    "time": 30,
+    "shouldNotifyHalfTime": true,
+    "shouldPlaySound": true
+  },
+  {
+    "message": "End",
+    "time": 5
+  },
+  {
+    "message": "Relax 10 seconds",
+    "time": 10
+  },
+  {
+    "message": "Next exercise is Wrist Stretch in 5 seconds",
+    "time": 10
+  },
+  {
+    "message": "Start",
+    "time": 30,
+    "shouldNotifyHalfTime": true,
+    "shouldPlaySound": true
+  },
+  {
+    "message": "End",
+    "time": 5
+  },
+  {
+    "message": "Relax 10 seconds",
+    "time": 10
+  },
+  {
+    "message": "Next exercise is Cat-Cow Stretch in 5 seconds",
+    "time": 10
+  },
+  {
+    "message": "Start",
+    "time": 30,
+    "shouldNotifyHalfTime": true,
+    "shouldPlaySound": true
+  },
+  {
+    "message": "End",
+    "time": 5
+  },
+  {
+    "message": "Relax 10 seconds",
+    "time": 10
+  },
+  {
+    "message": "Next exercise is Child Pose in 5 seconds",
+    "time": 10
+  },
+  {
+    "message": "Start",
+    "time": 30,
+    "shouldNotifyHalfTime": true,
+    "shouldPlaySound": true
+  },
+  {
+    "message": "End",
+    "time": 5
+  },
+  {
+    "message": "Relax 10 seconds",
+    "time": 10
+  },
+  {
+    "message": "Next exercise is Seated Forward Bend in 5 seconds",
+    "time": 10
+  },
+  {
+    "message": "Start",
+    "time": 30,
+    "shouldNotifyHalfTime": true,
+    "shouldPlaySound": true
+  },
+  {
+    "message": "Relax 10 seconds",
+    "time": 10
+  },
+  {
+    "message": "Next exercise is Butterfly Stretch in 5 seconds",
+    "time": 10
+  },
+  {
+    "message": "Start",
+    "time": 30,
+    "shouldNotifyHalfTime": true,
+    "shouldPlaySound": true
+  },
+  {
+    "message": "End",
+    "time": 5
+  },
+  {
+    "message": "Relax 10 seconds",
+    "time": 10
+  },
+  {
+    "message": "Next exercise is Quadriceps Stretch in 5 seconds",
+    "time": 10
+  },
+  {
+    "message": "Start",
+    "time": 30,
+    "shouldNotifyHalfTime": true,
+    "shouldPlaySound": true
+  },
+  {
+    "message": "End",
+    "time": 5
+  },
+  {
+    "message": "Relax 10 seconds",
+    "time": 10
+  },
+  {
+    "message": "Next exercise is Hamstring Stretch in 5 seconds",
+    "time": 10
+  },
+  {
+    "message": "Start",
+    "time": 30,
+    "shouldNotifyHalfTime": true,
+    "shouldPlaySound": true
+  },
+  {
+    "message": "End",
+    "time": 5
+  },
+  {
+    "message": "Relax 10 seconds",
+    "time": 10
+  },
+  {
+    "message": "Next exercise is Inner Thigh Stretch in 5 seconds",
+    "time": 10
+  },
+  {
+    "message": "Start",
+    "time": 30,
+    "shouldNotifyHalfTime": true,
+    "shouldPlaySound": true
+  },
+  {
+    "message": "End",
+    "time": 5
+  },
+  {
+    "message": "Relax 10 seconds",
+    "time": 10
+  },
+  {
+    "message": "Next exercise is Calf Stretch in 5 seconds",
+    "time": 10
+  },
+  {
+    "message": "Start",
+    "time": 30,
+    "shouldNotifyHalfTime": true,
+    "shouldPlaySound": true
+  },
+  {
+    "message": "End",
+    "time": 5
+  },
+  {
+    "message": "Relax 10 seconds",
+    "time": 10
+  },
+  {
+    "message": "Next exercise is Spinal twist in 5 seconds",
+    "time": 10
+  },
+  {
+    "message": "Start",
+    "time": 30,
+    "shouldNotifyHalfTime": true,
+    "shouldPlaySound": true
+  },
+  {
+    "message": "End",
+    "time": 5
+  },
+  {
+    "message": "Good job!",
+    "time": 5
+  }
+]
+
+export default list;
